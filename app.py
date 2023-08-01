99f0e0c818d76ec1927367396fd4d1b8c8e52298
BGQM <128929031+BGQM@users.noreply.github.com>
2023-08-01 15:26:54 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,59 @@
+import dash
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+import dash_auth
+import pandas as pd
+
+#VALID_USERNAME_PASSWORD_PAIRS = {'hello': 'world', 'bill':'pepper'}
+colors = {'background': '#0086b3','text': '#ffffff'}#blue background and white text - Note: This matches the YETI theme!
+
+app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.YETI, dbc.icons.FONT_AWESOME])
+
+#auth = dash_auth.BasicAuth(app, VALID_USERNAME_PASSWORD_PAIRS)
+
+server = app.server
+
+sidebar = dbc.Nav(
+            [
+                dbc.NavLink([
+                    html.Div(page["name"], className="ms-2"),],
+                    href=page["path"],
+                    active="exact",
+                )
+                for page in dash.page_registry.values()
+            ],
+            vertical=True,
+            pills=True,
+            className="bg-light",
+)
+
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col(html.Div('CBC Analytics Dashboard',
+                         style={'fontSize':24, 
+                         'fontWeight' : 'bold',
+                         'textAlign':'center', 
+                         'color' : colors['text'],
+                         'background-color' : colors['background']
+                         }))
+    ]),
+
+    html.Hr(),
+
+    dbc.Row(
+        [
+            dbc.Col(
+                [
+                    sidebar
+                ], xs=4, sm=4, md=2, lg=2, xl=2, xxl=2),
+
+            dbc.Col(
+                [
+                    dash.page_container
+                ], xs=8, sm=8, md=10, lg=10, xl=10, xxl=10)
+        ]
+    )
+], fluid=True)
+
+if __name__ == "__main__":
+    app.run(debug=False)
